703dca20e95b5dc5e5be8f29e90692e4a6e7c93c
Ayoobi salahudheen <ayoobisalahudheen@gmail.com>
2026-07-10 16:24:36 +0530

    navigation mobile view resolved

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,26 @@
 function initNavigation() {
-    console.log("🧭 Navigation Initialized");
+
+    const menuToggle = document.getElementById("menuToggle");
+    const nav = document.getElementById("nav");
+
+    if (!menuToggle || !nav) return;
+
+    menuToggle.addEventListener("click", () => {
+
+        nav.classList.toggle("active");
+        document.body.classList.toggle("menu-open");
+
+    });
+
+    document.querySelectorAll("#nav a").forEach(link => {
+
+        link.addEventListener("click", () => {
+
+            nav.classList.remove("active");
+            document.body.classList.remove("menu-open");
+
+        });
+
+    });
+
 }
